3c90e1fea8195b45d21e23e4cd7ccd2956072af8
Harshil0309 <harshilgupta562@gmail.com>
2023-06-04 00:13:41 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,107 +1,87 @@
-// FeedItem function
-// <FeedItem/> component
-
-import FeedItem from "./FeedItem";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function FeedList() {
-  //fetch will return a promise , convert it to json
-  // const number = 1;
-  // const posts = [{id : 1 , title : "First" , content : "lorem ipsum dolor set amet"},{id : 1 , title : "Second" , content : "lorem ipsum dolor set amet"},{id : 1 , title : "Third" , content : "lorem ipsum dolor set amet"},{id : 1 , title : "First" , content : "lorem ipsum dolor set amet"}]
+function App() {
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [clickedId, setClickedId] = useState(1);
+  const [clickedDetails, setClickedDetails] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setLoading(false);
+      setBlogs(data.data);
+    };
 
-  //const [data, setdata] = useState([]);
-  //const [loading, isLoading] = useState(true);
-  //   useEffect(()=>{
-  // fetch("https://jsonplaceholder.typicode.com/posts").then((result)=>{
-  //  // console.log(result)
-  //   result.json().then((resp)=>{
-  //     //console.log(resp);
-  //    setdata(resp)
+    fetchData();
+  }, []);
 
-  //   })
-  // })
-  //   },[])
-
-  // useEffect(() => {
-  //   // axios.get('https://jsonplaceholder.typicode.com/posts').then(res=>{
-  //   //   console.log(res)
-  //   //   setdata(res.data)
-  //   // })
-  //   // .catch(err=>{
-  //   //   console.log(err)
-  //   // })
-  //   const fetchData = async () => {
-  //     isLoading(true);
-  //     const data = await axios.get(
-  //       "https://jsonplaceholder.typicode.com/posts"
-  //     );
-  //     isLoading(false);
-  //     setdata(data.data);
-  //   };
-
-  //   fetchData();
-  // }, []);
-  // return (
-  //   //jsx
-  //   <div id="post-list">
-  //     <h1>Navbar</h1>
-  //     {loading ? (
-  //       <h1>Loading</h1>
-  //     ) : (
-  //       data.map((post) => {
-  //         return <FeedItem post={post} name="sohel" />;
-  //       })
-  //     )}
-  //   </div>
-  // );
-
-  //const [number, changeNumber] = useState(0);
-  // changeNumber(2)
-  // const [name, setName] = useState("");
-  // const [number, sum] = useState(0);
-  const [num1, setNum1] = useState(null);
-  const [num2, setNum2] = useState(null);
-
-  function renderContent() {
-    if (num1 == null && num2 == null) {
-      return <h1>Enter both</h1>;
-    } else if (num1 == null || num2 == null) {
-      return <h1>Enter one</h1>;
-    } else {
-      const ans = Number(num1) + Number(num2);
-      if (isNaN(ans)) {
-        return <h1>Enter valid input</h1>;
-      } else {
-        return <h1>{ans}</h1>;
-      }
-    }
+  function onHandleclick(id) {
+    setClickedId(id);
   }
+
+  useEffect(() => {
+    if (clickedId == null) return;
+    const fetch = async () => {
+      const newData = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${clickedId}`
+      );
+      setClickedDetails(newData.data);
+    };
+    fetch();
+  }, [clickedId]);
 
   return (
     <div>
-      <input type="string" onChange={(e) => setNum1(e.target.value)}></input>
-      <input type="string" onChange={(e) => setNum2(e.target.value)}></input>
-      {renderContent()}
-      {/* <h1>{number}</h1>
-      <button
-        onClick={() => {
-          changeNumber(number + 1);
+      <h1>Blogs</h1>
+      {loading && <h1>Loading...</h1>}
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
         }}
       >
-        Click me
-      </button> */}
-
-      {/* <input
-        type="string"
-        onChange={(e) => {
-          // console.log(e.target.value);
-          setName(e.target.value);
-        }}
-      ></input>
-      {/* <h1>Hello {name}</h1> */}
+        <div style={{ width: "100%" }}>
+          {blogs.map((blogItem, index) => {
+            return (
+              <h3
+                onClick={() => {
+                  onHandleclick(blogItem.id);
+                }}
+                key={blogItem.id}
+              >
+                {" "}
+                {index + 1} {") "}
+                {blogItem.title}
+              </h3>
+            );
+          })}
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "sticky",
+            top: "0",
+            height: "100vh",
+          }}
+        >
+          {clickedDetails == null ? (
+            <h4>Loading</h4>
+          ) : (
+            <div>
+              <h4>{clickedDetails.title}</h4>
+              <p>{clickedDetails.body}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default FeedList;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import FeedList from './App';
-import ToDo from "./ToDo";
-import ResumeBuilder from "./ResumeBuilder"
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* <ToDo /> */}
-    <ResumeBuilder/>
-  </React.StrictMode>
-);
+root.render(<App />);
